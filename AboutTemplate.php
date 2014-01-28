19dade241c22629e2b81cff4b5a9ac60003f8855
CurtisFenner <cfenneremail@gmail.com>
2014-01-28 09:51:56 -0500

    About Template improvements
    Changed markup to use pagewidth to set page width, instead of using the
    twocol div.
    
    Used margin-bottom to make a 5 pixel space between paragraphs within
    .pagewidth.
    
    Darkened the atom logo's atom

--- AboutTemplate.php
+++ AboutTemplate.php
@@ -18,27 +18,29 @@ $add_script = array();
 				<?php dynamic_sidebar('About Submenu') ?>
 		</div>
 		<div class="page_shadowline"></div>
-		<div id="twocolumns">
-			<?php 
-				$edit_attr = array(
-					'alt' => get_the_title(),
-					'title' => '',
-					'class' => 'floatright'
-					);
-				$post_image = get_the_post_thumbnail($post->ID,'pristine-custom-image-medium1', $edit_attr);					
-			?>
-			<?php
-				if(!empty($post_image)) {
-			 		echo $post_image;
-				}
-			?>
-			<?php the_content(); ?>
-			<?php endwhile; ?>
-				<!-- post navigation -->
-			<?php else: ?>
-				<!-- no posts found -->
-			<?php endif; ?>
-			<div class="spacer"></div>
+		<div class="pagewidth">
+			<div id="twocolumns">
+				<?php 
+					$edit_attr = array(
+						'alt' => get_the_title(),
+						'title' => '',
+						'class' => 'floatright'
+						);
+					$post_image = get_the_post_thumbnail($post->ID,'pristine-custom-image-medium1', $edit_attr);					
+				?>
+				<?php
+					if(!empty($post_image)) {
+				 		echo $post_image;
+					}
+				?>
+				<?php the_content(); ?>
+				<?php endwhile; ?>
+					<!-- post navigation -->
+				<?php else: ?>
+					<!-- no posts found -->
+				<?php endif; ?>
+				<div class="spacer"></div>
+			</div>
 		</div>
 	</div>
 <?php get_footer(); ?>
