1668a2ba131a88eb49ccc9d995607dd2e694ccc9
CurtisFenner <cfenneremail@gmail.com>
2014-02-26 17:40:10 -0500

    Fix Blog Page's Flow

--- page.php
+++ page.php
@@ -1,8 +1,14 @@
 <?php get_header(); ?>
-	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-		<div id="pagetitle">
-			<h1><?php the_title(); ?></h1>
-			<?php
+<div class="pagewidth">
+
+	<div class="twocolumns">
+		<div id="twocolumnbackground"></div>
+		<div id="leftcol">
+			<?php while ( have_posts() ) : the_post(); echo "HI!"; ?>
+
+			<div id="pagetitle">
+				<h1><?php the_title(); ?></h1>
+				<?php
 				$sub_links = get_post_meta(get_the_ID(),'sub-links',true);
 				$sub_links = explode(',',$sub_links);
 				
@@ -10,44 +16,38 @@
 					?>
 					<div class="subnav">
 						<ul>						
-						<?php
+							<?php
 							foreach($sub_links as $sub_link) {
 								echo "<li>$sub_link</li>";
 							}
-						?>
+							?>
 						</ul>
 					</div>										
 					<?php
 				}
-			?> 
-		</div>
-		<div class="pagewidth">
-			<div class="left">
-				<?php 
-					$edit_attr = array(
-						'alt' => get_the_title(),
-						'title' => '',
-						'class' => 'floatright'
-						);
-
-					$post_image = get_the_post_thumbnail($post->ID,'pristine-custom-image-medium1', $edit_attr);					
-				?>
-				<?php
-					if(!empty($post_image)) {
-				 		echo $post_image;
-					}
-				?>
-				<?php the_content(); ?>
-				<div class="spacer"></div>
-				<?php comments_template(); ?>
+				?> 
 			</div>
-	<?php endwhile; ?>
-		<!-- post navigation -->
-	<?php else: ?>
-		<!-- no posts found -->
-	<?php endif; ?>
-		<div class="right">
-			<?php get_sidebar(); ?>
-		</div>
+			<?php 
+			$edit_attr = array(
+				'alt' => get_the_title(),
+				'title' => '',
+				'class' => 'floatright'
+				);
+
+			$post_image = get_the_post_thumbnail($post->ID,'pristine-custom-image-medium1', $edit_attr);					
+			if(!empty($post_image)) {
+				echo $post_image;
+			}
+
+			the_content();
+			endwhile; ?>
+
 	</div>
+	<div id="rightcol">
+		Right.
+	</div>
+	<div class="clear"></div> <!-- this part is important! :D -->
+</div>
+</div>
+</div>
 <?php get_footer(); ?>
