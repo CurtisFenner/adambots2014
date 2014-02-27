7996fcb186fdf2da37253567d52f14921fabe134
CurtisFenner <cfenneremail@gmail.com>
2014-02-26 21:49:19 -0500

    Calendar Template

--- page.php
+++ page.php
@@ -7,8 +7,7 @@
 			<?php while ( have_posts() ) : the_post(); ?>
 
 					<h1><a href="<?php  the_permalink(); ?>"><?php the_title(); ?></a></h1>
-					<p>Written by <?php the_author_link(); ?> on <?php the_date(); ?></p>
-					<?php the_excerpt(); ?>
+					<?php the_content(); ?>
 					<hr>
 			<?php endwhile; ?>
 
