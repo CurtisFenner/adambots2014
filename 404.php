eaca16043657e53963593bd6922f5abb8be799ac
CurtisFenner <cfenneremail@gmail.com>
2014-02-27 15:38:38 -0500

    Simple 404

--- 404.php
+++ 404.php
@@ -1,3 +1,13 @@
 <?php get_header(); ?>
-    <img src="<?php bloginfo('template_directory'); ?>/res/img/404.png" width=100% >
+<div class="pagewidth">
+	<div class="onecolumn">
+		<h1 style="font-size:900%;">404</h1>
+		<div style="font-size:200%;">
+			This page doesn't exist here.<br>
+			Try going back, or looking through the navigation above.<br><br>
+			Sorry about that!
+		</div>
+	</div>
+</div>
+</div>
 <?php get_footer();
