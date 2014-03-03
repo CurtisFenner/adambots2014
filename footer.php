0325a6c1a1c1c6669df7b5d3290f75b1567228af
CurtisFenner <cfenneremail@gmail.com>
2014-03-03 15:04:13 -0500

    Up To Date With Site Copy

--- footer.php
+++ footer.php
@@ -1,14 +1,9 @@
 		<div id="footer">
-			<div style="top:50px;position:relative;margin:auto;text-align:center; width:950px;">
-				<span style="line-height:25px;">© Copyright 2005-2014 AdamBots. All Rights Reserved.</span><br/>
+			<div style="padding-top:30px;margin-left:auto;margin-right:auto;text-align:center; width:950px;">
+				<div style="line-height:25px">© Copyright 2005-2014 AdamBots. All Rights Reserved.</div>
 <div id="footerjustify">FIRST, FRC, FTC, FLL, JR.FLL, COOPERTITION, GRACIOUS&nbsp;PROFESSIONALISM, AERIAL&nbsp;ASSIST, ULTIMATE&nbsp;ASCENT, REBOUND&nbsp;RUMBLE, LOGOMOTION, BREAKAWAY, LUNACY, FIRST&nbsp;OVERDRIVE, RACK&nbsp;N&nbsp;ROLL, AIM&nbsp;HIGH, TRIPLE&nbsp;PLAY, FIRST&nbsp;FRENZY:&nbsp;RAISING&nbsp;THE&nbsp;BAR, STACK&nbsp;ATTACK, ZONE&nbsp;ZEAL, DIABOLICAL&nbsp;DYNAMICS, CO-OPERTITION&nbsp;FIRST, and DOUBLE&nbsp;TROUBLE are registered trademarks of United States Foundation for Inspiration and Recognition of Science and Technology AKA US FIRST&nbsp;CORPORATION NEW&nbsp;HAMPSHIRE 200&nbsp;Bedford Street Manchester NEW&nbsp;HAMPSHIRE 03101. USFIRST trademarks and logos used with permission as a USFIRST team.</div>
 			</div>
-			<br>
-			<br>
-			<br>
-			<br>
-			
-			The AdamBots' <em>FRC</em> Best Website Awards:<br>
+			<div style="margin-bottom:5px;margin-top:12px;">The AdamBots' <em>FRC&nbsp;</em> Best Website Awards:</div>
 			<img src="http://www.adambots.com/wp-content/themes/adambotsv6/images/websiteawards/12michigan.jpg" alt="Website Award 2012, Michigan Championship">
 			<img src="http://www.adambots.com/wp-content/themes/adambotsv6/images/websiteawards/12alamo.jpg" alt="Website Award 2012, Alamo Regional">
 			<img src="http://www.adambots.com/wp-content/themes/adambotsv6/images/websiteawards/12troy.jpg" alt="Website Award 2012, Troy District">
