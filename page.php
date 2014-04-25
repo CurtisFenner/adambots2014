6caa5b62ebd10fd7bae51e9728d779a6d1bf501a
CurtisFenner <cfenneremail@gmail.com>
2014-04-25 18:46:19 -0400

    Reconfig Default Template. Bug/w/e fix in Generic

--- page.php
+++ page.php
@@ -1,15 +1,28 @@
-<?php get_header(); ?>
+<?php
+
+//This is default renderer for pages (those without a tempalte set).
+//For posts' rendering, see SINGLE.PHP.
+//This should ideally never be visible, but we still want it to look nice.
+
+get_header();
+?>
+
 <div class="pagewidth">
-	<!-- A pagetype here might be a good idea but would not contain subnav, like LamBot page -->
-			<?php while ( have_posts() ) : the_post(); ?>
-					<h1><a href="<?php  the_permalink(); ?>"><?php the_title(); ?></a></h1>
-					<?php the_content(); ?>
-					<hr>
-			<?php endwhile; ?>
-	<div class="clear"></div> <!-- this part is important! :D -->
+	<div id="pagetype" style="background-color:white;" class="blacktext">
+		<div style="font-size:1.5em;line-height:40px;">
+			Hello from the 
+			<div style="display:inline-block;padding:0;margin:0;height:40px;">
+				<img style="margin:0" height="28" alt="AdamBots" src="<?php bloginfo("template_directory")?>/res/img/adambots_text_outlined.png"/>
+			</div>
+		</div>
+		<div class="background">
+			<img style="margin:-10px;margin-right:4px;" src="<?php bloginfo("template_directory")?>/res/img/navAtomThin.png" alt="AdamBots Team 245" height="85">
+		</div>
+		This page doesn't have a template set. That doesn't seem quite right.
+	</div>
 </div>
-</div>
-<?php get_footer(); ?>
 
-
-<!-- OH MY GOODNESS I MESSED THIS UP -->
+<?php
+$showtitle = true;
+include "genericbottom.php";
+?>
