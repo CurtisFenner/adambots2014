05baac4f8343a1fdb5819b7112d8d3d1821eb258
CurtisFenner <cfenneremail@gmail.com>
2014-04-25 17:42:45 -0400

    Reconfig/valid. FIRST, Homepage, LamBot, OCCRA

--- first.php
+++ first.php
@@ -2,48 +2,23 @@
 /*
 Template Name: FIRST
 */
-
-//Add stylesheets to this page
-global $add_style;
-$add_style = array();
-
-//Add scripts to this page
-global $add_script;
-$add_script = array();
-
 ?>
-<?php get_header(); ?>
-	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+<?php
+get_header();
+?>
+
 		<div class="pagewidth">
-			<div id="pagetype" style="background-color:#0166B3;">
+			<div id="pagetype" style="background-color:#0065B3;">
 				<div style="font-size:1.5em;line-height:40px;color:white;">
 					<em>FIRST</em>
 					<span style="vertical-align:baseline;font-size:50%;line-height:50%;"><em>For Inspiration and Recognition of Science and Technology</em></span>
 				</div>
-				<div class="background"><img src="<?php bloginfo('template_directory'); ?>/res/img/firstmono.png" height="60" alt="FIRST logo"></div>
+				<div class="background">
+					<img src="<?php bloginfo('template_directory'); ?>/res/img/firstmono.png" height="60" alt="FIRST logo">
+				</div>
 				<?php dynamic_sidebar('FIRST Submenu') ?>
 			</div>
-			<div class="twocolumns">
-				<?php 
-					$edit_attr = array(
-						'alt' => get_the_title(),
-						'title' => '',
-						'class' => 'floatright'
-						);
-					$post_image = get_the_post_thumbnail($post->ID,'pristine-custom-image-medium1', $edit_attr);					
-				?>
-				<?php
-					if(!empty($post_image)) {
-				 		echo $post_image;
-					}
-				?>
-				<?php the_content(); ?>
-				<?php endwhile; ?>
-					<!-- post navigation -->
-				<?php else: ?>
-					<!-- no posts found -->
-				<?php endif; ?>
-			</div>
 		</div>
-	</div>
-<?php get_footer(); ?>
+
+<?php include "genericbottom.php"; ?>
--- occra.php
+++ occra.php
@@ -2,47 +2,25 @@
 /*
 Template Name: OCCRA
 */
-
-//Add stylesheets to this page
-global $add_style;
-$add_style = array();
-
-//Add scripts to this page
-global $add_script;
-$add_script = array();
-
 ?>
-<?php get_header(); ?>
-	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-		<div class="pagewidth">
-			<div id="pagetype" style="background-color:#FFA411;">
-				<h2 style="padding:0;color:#FFF;height:80px;line-height:80px;font-size:50px;">
-					<em>OCCRA</em> <span style="font-size:50%">Oakland County Competitive Robotics Association</span>
-				</h2>
-				<div class="background"><img src="<?php bloginfo('template_directory'); ?>/res/img/occralogo.png" style="margin:-7px;" height="80"></div>
-				<?php dynamic_sidebar('OCCRA Submenu') ?>
-			</div>
 
-            <?php 
-                $edit_attr = array(
-                    'alt' => get_the_title(),
-                    'title' => '',
-                    'class' => 'floatright'
-                    );
-                $post_image = get_the_post_thumbnail($post->ID,'pristine-custom-image-medium1', $edit_attr);					
-            ?>
-            <?php
-                if(!empty($post_image)) {
-                    echo $post_image;
-                }
-            ?>
-            <?php the_content(); ?>
-            <?php endwhile; ?>
-                <!-- post navigation -->
-            <?php else: ?>
-                <!-- no posts found -->
-            <?php endif; ?>
+<?php get_header();
+?>
 
+
+<div class="pagewidth">
+	<div id="pagetype" style="background-color:#FFA411;">
+		<h2 style="padding:0;color:#FFF;height:80px;line-height:80px;font-size:50px;">
+			<em>OCCRA</em> <span style="font-size:50%">Oakland County Competitive Robotics Association</span>
+		</h2>
+		<div class="background">
+			<img src="<?php bloginfo('template_directory'); ?>/res/img/occralogo.png" alt="OCCRA Logo" style="margin:-7px;" height="80">
 		</div>
+		<?php dynamic_sidebar('OCCRA Submenu') ?>
 	</div>
-<?php get_footer(); ?>
+</div>
+
+
+<?php
+include("genericbottom.php");
+?>
