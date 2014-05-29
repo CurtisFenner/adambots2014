09ee7474bc9ba5396b706d45d1bdd27e8da851a1
CurtisFenner <cfenneremail@gmail.com>
2014-05-29 19:31:32 -0400

    Minor Countdown Improvements

--- js/countdown.js
+++ js/countdown.js
@@ -1,14 +1,14 @@
 function pluralUnit(num,tex) {
-	return "<span style='font-weight:bold;color:#ffd800;padding-left:10px;width:30px;display:inline-block;text-align:center;font-size:100%;'>" + num + "</span> " + tex + (num != 1 ? "s" : "") + " ";
+	return "<span style='display:inline-block;'><span style='font-weight:bold;color:#ffd800;padding-left:10px;width:30px;display:inline-block;text-align:center;font-size:100%;'>" + num + "</span> " + tex + (num != 1 ? "s" : "") + " " + "</span>";
 }
 
 function updateCountdown() {
 	var d = Date.now();
 	var eventtime = new Date("June 21 2014 9:00 AM EDT").getTime(); //Date.UTC(2014,0,4,15,30,0); //UTC time (6pm local [ET])
-	var eventName = "the Rochester <em>Relay for Life&nbsp;</em> Relay";
+	var eventName = "Rochester ACS <em>Relay for Life&nbsp;</em>";
 	var rem = -Date.now() + eventtime;
 	if (rem <= 0) {
-		document.getElementById("countdown").innerHTML = "<span style='color:#ffd800;'>" + eventName + "</span> is going on!";
+		document.getElementById("countdown").innerHTML = "The <span style='color:#ffd800;'>" + eventName + "</span> is going on!";
 		//document.getElementById("countdown").innerHTML = "<span style='color:#ffd800;'>Let's go robots! Another awesome <em>FRC&nbsp;</em> World Championship is underway!</span>";
 		return;
 	} else {
@@ -28,7 +28,7 @@ function updateCountdown() {
 		rem = rem % minSec;
 		var seconds = Math.floor(rem);
 
-		document.getElementById("countdown").innerHTML = "Countdown to <span style='color:#ffd800;font-size:100%;'>" + eventName + "</span>:" + (weeks != 0 ? pluralUnit(weeks,"week") : "") + pluralUnit(days,"day") + pluralUnit(hours,"hour") + pluralUnit(minutes,"minute") + pluralUnit(seconds,"second");
+		document.getElementById("countdown").innerHTML = "Countdown to the <span style='color:#ffd800;font-size:100%;'>" + eventName + "</span>:" + (weeks != 0 ? pluralUnit(weeks,"week") : "") + pluralUnit(days,"day") + pluralUnit(hours,"hour") + pluralUnit(minutes,"minute") + pluralUnit(seconds,"second");
 	}
 	
 }
