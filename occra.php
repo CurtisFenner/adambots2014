22a008ff8a41658df955cf54030e327abb166c60
CurtisFenner <cfenneremail@gmail.com>
2014-06-04 17:11:47 -0400

    OCCRA Background Cleanup

--- occra.php
+++ occra.php
@@ -11,8 +11,8 @@ get_header();
 
 <div class="pagewidth">
 	<div id="pagetype" style="background-color:#FFA411;">
-		<h2 style="padding:0;color:#FFF;height:80px;line-height:80px;font-size:50px;">
-			<em>OCCRA</em> <span style="font-size:50%">Oakland County Competitive Robotics Association</span>
+		<h2 style="padding:0;color:#FFF;height:2em;line-height:2em;font-size:2em;">
+			<em>OCCRA</em> <span class=deskonly style="font-size:50%">Oakland County Competitive Robotics Association</span>
 		</h2>
 		<div class="background">
 			<img src="<?php bloginfo('template_directory'); ?>/res/img/occralogo.png" alt="OCCRA Logo" style="margin:-7px;" height="80">
