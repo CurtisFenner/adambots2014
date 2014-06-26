62a8d0fffd1000f9741744c98acc972a9c907805
CurtisFenner <cfenneremail@gmail.com>
2014-06-26 17:50:15 -0400

    Search Performance + Quality

--- search.php
+++ search.php
@@ -1,4 +1,6 @@
 <?php
+header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
 $qvrs = $wp_query -> query_vars;
 $searchterms = $qvrs["search_terms"];
 function lengthCompare($a,$b){
@@ -9,36 +11,92 @@ usort($searchterms,'lengthCompare'); // Longest first
 function firstCompare($a,$b) {
 	return $b[0] - $a[0]; // Most first
 }
+
+function tim() {
+	return microtime(TRUE);
+}
+
+include("searchresult.php");
+
 ?>
 
-<?php get_header(); ?>
+<?php get_header(); flush() ?>
 
 <div class="pagewidth">
 	<div class="onecolumn">
 		<h1>Search results for &ldquo;<?php the_search_query();?>&rdquo;</h1>
-		<hr>
 		<?php
 		$results = array();
 		$resultnum = 0;
 		//query_posts('showposts=999');
 		query_posts($query_string . '&showposts=999');
+		$t_have = tim();
+		$t_res = 0;
 		while ( have_posts() ) {
 			the_post();
-			include("searchresult.php");
-			$results[$resultnum] = array($count,$section);
+			$t_res_s = tim();
+			$results[$resultnum] = make_result($searchterms);
+			$t_res += tim() - $t_res_s;
 			$resultnum++;
-			//echo "\n\n\n"; echo $section; echo "\n\n\n";
 		}
+		$t_have = tim() - $t_have;
+		echo "Showing " . min(25,$resultnum) . " of " . $resultnum . " results";
+		?>
+		<hr>
+		<?php
+		$t_sort = tim();
 		usort($results,'firstCompare');
+		$t_sort = tim() - $t_sort;
+		$t_echo = tim();
+		// From `make_result()`
+		$t_init = 0;
+		$t_regex = 0;
+		$t_bold = 0;
+		$t_dots = 0;
+		$t_end = 0;
+		//
 		$average = 0;
 		for ($i = 0; $i < $resultnum; $i++) {
-			$average += $results[$i][0];
+			$res = $results[$i];
+			$average += $res[0];
+			//
+			$t_init += $res[2];
+			$t_regex += $res[3];
+			$t_bold += $res[4];
+			$t_dots += $res[5];
+			$t_end += $res[6];
 		}
 		$average /= $resultnum;
-
-		for ($i = 0; $i < $resultnum; $i++) {
-			echo $results[$i][1];
+		for ($i = 0; $i < $resultnum && $i < 25; $i++) {
+			$relevance = $results[$i][0] - $average;
+			$relevance = $relevance / $average;
+			$relevance = tanh($relevance);
+			if ($relevance <= 0) {
+				$relevance = ($relevance + 1) * .25;
+			} else {
+				$relevance = $relevance * .75 + .25;
+			}
+			$relevance = floor($relevance * 100) . "%";
+			$k = str_replace('<num>', '<span style=float:right;>' . ($i + 1) . '.</span>', $results[$i][1]);
+			$k = str_replace('<rel>',$relevance,$k);
+			echo $k;
+			flush();
 		}
+		$t_echo = tim() - $t_echo;
+
+		echo "<div style=display:none;>";
+		echo "have_posts() Loop: $t_have seconds<br>";
+		echo "(in have_posts() loop) Search result: $t_res seconds<br>";
+		echo "Sort time: $t_sort seconds<br>";
+		echo "Echo time: $t_echo seconds<br>";
+
+		echo "<b>Result Time Breakdown:</b><br>";
+		echo "Init time: $t_init seconds<br>";
+		echo "Regex time: $t_regex seconds<br>";
+		echo "Bold time: $t_bold seconds<br>";
+		echo "Dots time: $t_dots seconds<br>";
+		echo "End time: $t_end seconds<br>";
+		echo "</div>";
 		?>
 	</div>
 </div>
--- searchresult.php
+++ searchresult.php
@@ -5,67 +5,80 @@
 
 ?>
 <?php
-$section = '<section style="font-size:16px;"><h2><a href="';
-$section .= get_permalink($post->ID) . '" style="font-size:24px;text-decoration:underline;">';
-$section .= get_the_title() . "</a></h2><br>";
-$str = get_the_content();
+function make_result($searchterms) {
+	$t_init = tim();
 
-$year = the_date("Y"," ","",FALSE);
-$counter = strtolower($str . str_repeat($year,3));
-$titlecounter = strtolower(get_the_title() . $year);
-$count = 0;
+	$section = '<section style="font-size:16px;"><num><h2><a href="';
+	$section .= get_permalink($post->ID) . '" style="font-size:24px;text-decoration:underline;">';
+	$section .= get_the_title() . "</a></h2><br>";
+	$str = get_the_content();
 
+	$year = the_date("Y"," ","",FALSE);
+	$counter = strtolower($str . str_repeat($year,3));
+	$titlecounter = strtolower(get_the_title() . $year);
+	$count = 0;
 
-$str = str_replace("\n",'',$str);
-$str = preg_replace('/<\/?(p|h1|h2|h3|h4|h5|div)[^>]*>/i' ,"\n",$str);
-$str = str_replace('<br />',"\n",$str);
-$str = str_replace('<hr />',"\n",$str);
-$str = str_replace('<hr>',"\n",$str);
-$str = strip_tags($str);
-$str = str_replace('[bigdivider]',"\n",$str);
-$str = str_replace('[right]','',$str);
-$str = str_replace('[/right]','',$str);
-$str = str_replace('[left]','',$str);
-$str = str_replace('[/left]','',$str);
-$str = trim(preg_replace("/\n\s*/","\n",$str));
-for ($i = 0; $i < count($searchterms); $i++) {
-	$str = preg_replace('/' . $searchterms[$i] . '/i','<b>$0</b>',$str);
-	$lowterm = strtolower($searchterms[$i]);
-	$count += substr_count($counter,$lowterm);
-	$count += substr_count($titlecounter,$lowterm) * 10;
-}
-//$str now has bolded search terms.
+	$t_init = tim() - $t_init;
+	$t_regex = tim();
 
+	$str = str_replace("\n",'',$str);
+	$str = preg_replace('/<\/?(p|h1|h2|h3|h4|h5|div)[^>]*>/i' ,"\n",$str);
+	$str = str_replace('<br />',"\n",$str);
+	$str = str_replace('<hr />',"\n",$str);
+	$str = str_replace('<hr>',"\n",$str);
+	$str = strip_tags($str);
+	$str = str_replace('[bigdivider]',"\n",$str);
+	$str = str_replace('[right]','',$str);
+	$str = str_replace('[/right]','',$str);
+	$str = str_replace('[left]','',$str);
+	$str = str_replace('[/left]','',$str);
+	$str = trim(preg_replace("/\n\s*/","\n",$str));
 
-// Initial ellipses; keep 50 characters before first bold term
-$begin = strpos($str,"<b");
-if ($begin !== FALSE) {
-	$begin = $begin-50;
-	if ($begin > 0) {
-		$str = "&hellip;" . substr($str,$begin);
+	$t_regex = tim() - $t_regex;
+	$t_bold = tim();
+
+	for ($i = 0; $i < count($searchterms); $i++) {
+		$str = preg_replace('/' . $searchterms[$i] . '/i','<b>$0</b>',$str);
+		$lowterm = strtolower($searchterms[$i]);
+		$count += substr_count($counter,$lowterm);
+		$count += substr_count($titlecounter,$lowterm) * 10;
 	}
-}
 
-$strby = explode("</b>",$str,13); //Maximum of 12 bolded terms. We throw out the 13th+.
-if (count($strby) == 1) {
-	$str = "&hellip;";
-	// No terms bolded
-} else {
-	$newby = array_slice($strby,0,12);
-	$str = implode('</b>',$newby);
-	if (count($strby) > 12) {
-		$str .= '</b>';
+	$t_bold = tim() - $t_bold;
+	$t_dots = tim();
+	//$str now has bolded search terms.
+
+	// Initial ellipses; keep 50 characters before first bold term
+	$begin = strpos($str,"<b");
+	if ($begin !== FALSE) {
+		$begin = $begin-50;
+		if ($begin > 0) {
+			$str = "&hellip;" . substr($str,$begin);
+		}
 	}
-	$str .= '&hellip;';
+
+	$strby = explode("</b>",$str,13); //Maximum of 12 bolded terms. We throw out the 13th+.
+	if (count($strby) == 1) {
+		$str = "&hellip;";
+		// No terms bolded
+	} else {
+		$newby = array_slice($strby,0,12);
+		$str = implode('</b>',$newby);
+		if (count($strby) > 12) {
+			$str .= '</b>';
+		}
+		$str .= '&hellip;';
+	}
+	$t_dots = tim() - $t_dots;
+	$t_end = tim();
+	$str = substr($str,0, strrpos($str,"</b>") + 60);
+	$str = substr($str,0,strrpos($str," ")) . '&hellip;';
+	$str = preg_replace('/<\/b>([^<>]{40}\S*)[\s\S]*?(\S*[^<>]{50})<b>/','</B>$1 &hellip; $2<B>',$str,11);
+	$str = str_replace("\n","<br>",$str);
+	$str = str_replace("FIRST","<em>FIRST</em>",$str);
+	$str = str_replace("FRC","<em>FRC</em>",$str);
+	$str = str_replace("OCCRA","<em>OCCRA</em>",$str);
+	$section = $section . $str . "<br><br><p class=dim style=font-style:italic;>Relevance: <b><rel></b></p></section><hr>";
+	return array($count * (500 / (strlen($counter) + 20) + 1),$section, $t_init,$t_regex,$t_bold,$t_dots,tim() - $t_end);
 }
-$str = substr($str,0, strrpos($str,"</b>") + 60);
-$str = substr($str,0,strrpos($str," ")) . '&hellip;';
-$str = preg_replace('/<\/b>([^<>]{40}\S*)[\s\S]*?(\S*[^<>]{50})<b>/','</B>$1 &hellip; $2<B>',$str,10);
-$str = str_replace("\n","<br>",$str);
-$str = str_replace("FIRST","<em>FIRST</em>",$str);
-$str = str_replace("FRC","<em>FRC</em>",$str);
-$str = str_replace("OCCRA","<em>OCCRA</em>",$str);
-$section = $section . $str . "<br><br><p class=dim style=font-style:italic;>Relevance: <b>$count</b></p></section><hr>";
-
-
 ?>
