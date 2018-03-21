77ca673fffa469be9f7e540ab8138e0f763bb4e0
Curtis Fenner <cwfenner@umich.edu>
2018-03-21 13:44:10 -0400

    Add send.php

---
+++ send.php
@@ -0,0 +1,29 @@
+<?php
+	//This page receives messages from Contact Us and mails them to a small list of people.
+	//This list should probably be drawn from somewhere more easily editable...
+	error_reporting(E_NOTICE);
+		function valid_email($str)
+		{
+			return filter_var($str,FILTER_VALIDATE_EMAIL);
+			//return ( ! preg_match("/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix", $str)) ? FALSE : TRUE;
+		}
+		if($_POST['name']!='' && $_POST['email']!='' && valid_email($_POST['email'])==TRUE && strlen($_POST['comment'])>1)
+		{//drumrs@aol.com
+			$to = "email1@example.com,email2@example.com";
+			$headers =  'From: noreply@adambots.com\r\n' .
+				'Reply-To: '.$_POST['email'].'' . "\r\n" .
+				'X-Mailer: PHP/' . phpversion();
+			$subject = "AdamBots.com 'Contact Us' - Message from \"" . $_POST['email'] . '"';
+			$message = htmlspecialchars($_POST['name'] . " says:\n" . $_POST['comment'] . "\n\nPlease send reply to: " . $_POST['email']);
+			if(mail($to, $subject, $message, $headers))
+			{
+				echo 1; //SUCCESS
+			}
+			else {
+				echo 2; //FAILURE - server failure
+			}
+		}
+		else {
+			echo 3; //FAILURE - not valid email
+		}
+?>
